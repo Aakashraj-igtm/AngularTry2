0c3335566ec570940b56a55d07378e16aec43434
Aakashraj-igtm <saiaakashekbote@gmail.com>
2021-03-28 14:47:52 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    LoginComponentComponent,
+    SignupComponentComponent
   ],
   imports: [
     BrowserModule
